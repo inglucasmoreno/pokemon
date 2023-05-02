36252a9b42ec54569314dc5e00ecf8ccae09731b
Moreno Lucas Omar <ing.lucasmoreno@gmail.com>
2023-05-02 04:27:25 -0300

    Bug fixed

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -14,7 +14,7 @@ export const PokemonCard: FC<Props> = ({ pokemon }) => {
   const router = useRouter();
 
   const onClick = () => {
-    router.push(`/pokemon/${ pokemon.name }`);
+    router.push(`/name/${ pokemon.name }`);
   }
 
   return (
